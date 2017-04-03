b2e3c33f385d130639a327681e4e52a2d30aca31
GYCHEN <gy.chen@gms.nutc.edu.tw>
2017-04-03 10:51:21 +0800

    implement hiddyoutu component

--- src/components/hiddyoutu.js
+++ src/components/hiddyoutu.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
+import YouTuBe from 'react-youtube';
 
 
 class HiddYoutu extends Component {
 
+  constructor(props) {
+    super(props);
+
+    this.on_ready = this.on_ready.bind(this);
+  }
+
+  on_ready(event) {
+    event.target.playVideo();
+  }
+
   render() {
     const { match } = this.props;
+    const youtube_id = match.params.youtube_id;
 
     return (
-      <div>
-        Hello {match.params.youtube_id}
+      <div className='hiddyoutu'>
+        <YouTuBe
+          videoId={youtube_id}
+          onReady={this.on_ready} />
       </div>
     );
   }
