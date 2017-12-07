4e05b0198ea5bd00b711be85d8cc477c847917fb
gychen <gy.chen@gms.nutc.edu.tw>
2017-12-07 17:40:09 +0800

    add todo

---
+++ src/component/HiddenYoutubePlayer.js
@@ -0,0 +1,28 @@
+/**
+ * HiddenYoutubePlayer
+ * 
+ * Provide abilities to play youtube video via vidoe Id
+ * 
+ * TODOs:
+ *   - load Youtube API.
+ *   - play Youtube video when video id prop changed.
+ *   - provide player functions, to let other can manipulate it.
+ *      - play
+ *      - pause
+ *   - provide settings:
+ *      - autoplay: autoplay video when video Id changed or the player initlalized.
+ */
+import React, { Component } from 'react';
+
+
+class HiddenYoutubePlayer extends Component {
+
+    render() {
+        return (
+            <div>
+            </div>
+        );
+    }
+}
+
+export default HiddenYoutubePlayer;
