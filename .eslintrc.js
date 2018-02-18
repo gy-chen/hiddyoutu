5441722c62405fd8e84e86c67fec69cfce6ca94b
GYCHEN <gy.chen@gms.nutc.edu.tw>
2018-02-18 15:47:53 +0800

    use eslint-config-react-app

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,38 +1,3 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-        ],
-    "parserOptions": {
-        "ecmaFeatures": {
-            "experimentalObjectRestSpread": true,
-            "jsx": true
-        },
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-        "indent": [
-            "error",
-            4
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
-    }
+  "extends": "react-app"
 };
